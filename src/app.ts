6d329c449920c6e103521d5c30b6622100c0c75f
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-07-27 16:14:25 +0300

    rendered template tag content

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hello world fro TypeScript');
+class DragAndDrop {
+  templateElement: HTMLTemplateElement;
+  baseElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+ constructor() {
+   this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+   this.baseElement = document.getElementById('app')! as HTMLDivElement;
+
+   const importedNode = document.importNode(this.templateElement.content, true);
+   this.element = importedNode.firstElementChild as HTMLFormElement;
+   this.attach();
+ } 
+
+ private attach() {
+   this.baseElement.insertAdjacentElement('afterbegin', this.element);
+ }
+}
+
+const dropProject = new DragAndDrop();
